0b70dbc9cefc543a57230519f93fd1cf58ba8785
leepointofv <lekysu@naver.com>
2023-03-27 11:02:34 +0900

    Initial Commit

---
+++ gitest/gitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitestApp.swift
+//  gitest
+//
+//  Created by 이경수 on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct gitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
